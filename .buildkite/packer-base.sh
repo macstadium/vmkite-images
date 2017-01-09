febf3affd426127e33900c8e3282635a448b09c5
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-09 19:30:52 +1100

    Add output_dir param to base build

--- .buildkite/packer-base.sh
+++ .buildkite/packer-base.sh
@@ -5,21 +5,21 @@ installer_path=$(buildkite-agent meta-data get installer_path)
 version=$(basename "$installer_path" | cut -d_ -f3)
 installer_build=$(basename "$installer_path" | cut -d_ -f4)
 
+build_hash() {
+  find prepare_iso/ "$installer_build" -type f -print0 | xargs -0 shasum | awk '{print $1}' | sort | shasum | awk '{print $1}'
+}
+
+output_cache_path="${cache_dir}/output/$(build_hash)-$(version)-$(installer_build)"
+
 echo "+++ Running :packer: build for :mac: $version ($installer_build)"
 packer build \
 	-var iso_url="$installer_path" \
 	-var version="$version" \
 	-var packer_headless=true \
+	-var output_dir="$output_cache_path" \
 	macos.json
 
-build_hash() {
-  find prepare_iso/ "$installer_build" -type f -print0 | xargs -0 shasum | awk '{print $1}' | sort | shasum | awk '{print $1}'
-}
+vmx_path=$(ls -1 $output_cache_path/*.vmx)
+echo "+++ Built VMX $vmx_path"
 
-output_cache_path="${cache_dir}/output/$(build_hash)"
-
-mkdir -p "$output_cache_path"
-cp -a "output/$version/*" "$output_cache_path/"
-
-echo "--- Output VMX files"
-ls -al "$output_cache_path"
+buildkite-agent meta-data set base_vmx_path "$vmx_path"
---
+++ .buildkite/packer-buildkite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+base_vmx_path=$(buildkite-agent meta-data get base_vmx_path)
+
+echo "+++ Running :packer: build for :buildkite:"
+echo $base_vmx_path
+
+# packer build \
+# 	-var iso_url="$installer_path" \
+# 	-var version="$version" \
+# 	-var packer_headless=true \
+# 	macos.json
