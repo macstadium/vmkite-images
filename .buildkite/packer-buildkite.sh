797ccdff75b85aec8ea059f91db56c7c35c2a5ad
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-09 20:52:09 +1100

    Add buildkite packer template

--- .buildkite/packer-buildkite.sh
+++ .buildkite/packer-buildkite.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-base_vmx_path=$(buildkite-agent meta-data get base_vmx_path)
+vmx_path=$(buildkite-agent meta-data get base_vmx_path)
 
 echo "+++ Running :packer: build for :buildkite:"
-echo $base_vmx_path
+echo $vmx_path
 
-# packer build \
-# 	-var iso_url="$installer_path" \
-# 	-var version="$version" \
-# 	-var packer_headless=true \
-# 	macos.json
+packer build \
+	-var vmx_path="$vmx_path" \
+	-var packer_headless=true \
+	macos-buildkite.json
