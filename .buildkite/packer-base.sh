ad89f9616991076521e1438e7aebfa7cc8e08c03
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-09 20:24:33 +1100

    Fix incorrect output_dir in packer template

--- .buildkite/packer-base.sh
+++ .buildkite/packer-base.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 cache_dir="/usr/local/var/buildkite-agent/cache"
 installer_path=$(buildkite-agent meta-data get installer_path)
 version=$(basename "$installer_path" | cut -d_ -f3)
-installer_build=$(basename "$installer_path" | cut -d_ -f4)
 
 build_hash() {
   find prepare_iso/ "$installer_path" -type f -print0 | xargs -0 shasum | awk '{print $1}' | sort | shasum | awk '{print $1}'
@@ -15,12 +14,12 @@ output_cache_path="${cache_dir}/output/$(build_hash)"
 if [[ -d "$output_cache_path" ]] ; then
 	echo "Output is in cache already, skipping building"
 else
-	echo "--- Running packer for base $version ($installer_build)"
+	echo "--- Running packer for base :mac: $version"
 	packer build \
 		-var iso_url="$installer_path" \
 		-var version="$version" \
 		-var packer_headless=true \
-		-var output_dir="$output_cache_path" \
+		-var packer_output_dir="$output_cache_path" \
 		macos.json
 fi
 
