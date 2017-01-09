658bc192bcf8852fe18fc108871b2636ca6130b5
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-09 18:19:01 +1100

    Drop env vars for SUDO_UID/GID

--- .buildkite/prepare-sierra.sh
+++ .buildkite/prepare-sierra.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 
 echo "--- :mac: Preparing installer for macOS Sierra"
-sudo SUDO_UID="$UID" SUDO_GID="$GID" \
-  prepare_iso/prepare_iso.sh "/Applications/Install macOS Sierra.app" installers
+sudo prepare_iso/prepare_iso.sh "/Applications/Install macOS Sierra.app" installers
