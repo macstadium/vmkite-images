afaa6222210c1a8215cd643510bdb21b16a11354
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-11 12:07:01 +1100

    Add vmkite scripts to buildkite image

--- scripts/buildkite/agent.sh
+++ scripts/buildkite/agent.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo hello world
+set -e
+set -o pipefail
+set -u
+
+BUILDKITE_RELEASE="https://github.com/buildkite/agent/releases/download/v3.0-beta.16/buildkite-agent-darwin-amd64-3.0-beta.16.tar.gz"
+
+install_buildkite() {
+  echo "Installing buildkite-agent"
+  curl -Lfs -o buildkite.tar.gz "$BUILDKITE_RELEASE"
+  mkdir -p "$HOME/buildkite-agent"
+  tar -xzf buildkite.tar.gz -C "$HOME/buildkite-agent"
+  sudo mkdir -p /usr/local/bin
+  sudo ln -snf "$HOME/buildkite-agent/buildkite-agent" /usr/local/bin/
+}
+
+install_launchd_daemon() {
+  local script="vmkite-buildkite-agent.sh"
+  local plist="com.macstadium.vmkite-buildkite-agent.plist"
+  echo "Installing launchd service"
+  sudo chmod 0755 "/usr/local/bin/$script"
+  sudo launchctl load "/Library/LaunchDaemons/$plist"
+}
