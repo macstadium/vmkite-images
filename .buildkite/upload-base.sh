6a7c7c33509ece3f8328bffb2cc6ae1f1ec8b6b7
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-18 20:05:00 +1100

    Use correct directory basename

--- .buildkite/upload-base.sh
+++ .buildkite/upload-base.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 vmx_path=$(buildkite-agent meta-data get base_vmx_path)
 vmx_dir=$(dirname "$vmx_path")
-vm_image="macos-base-10.12-$(date +'%Y%m%d-%H%M%S')"
+vm_image="vmkite-base-macOS-10.12-$(date +'%Y%m%d-%H%M%S')"
 
 echo "+++ Uploading to $vm_image"
 find "$vmx_dir" -type f -print -exec govc datastore.upload {} "$vm_image"/{} \;
