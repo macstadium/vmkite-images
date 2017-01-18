e5f2336ce8083ee40d9a93697bf52bac98040d9c
Lachlan Donald <lachlan@strongwater.com.au>
2017-01-18 21:24:28 +1100

    Use correct ERROR trap syntax

--- .buildkite/02-build-base.sh
+++ .buildkite/02-build-base.sh
@@ -18,7 +18,7 @@ cleanup() {
 	rm -rf "$output_cache_path"
 }
 
-trap cleanup error
+trap cleanup ERROR
 
 if [[ -d "$output_cache_path" ]] ; then
 	echo "Output is in cache already, skipping building"
