7bae8a63593ec73ce0ece507813045fc7b255a10
Lachlan Donald <lachlan@strongwater.com.au>
2017-03-02 13:28:48 +1100

    Add back in the buildkite step, change base image path

--- .buildkite/03-build-buildkite.sh
+++ .buildkite/03-build-buildkite.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-# vmx_path=$(buildkite-agent meta-data get base_vmx_path)
-# osx_version=$(buildkite-agent meta-data get base_osx_version)
+vmx_path=$(buildkite-agent meta-data get base_vmx_path)
+osx_version=$(buildkite-agent meta-data get base_osx_version)
 
-# echo "+++ :packer: Building :buildkite: image from base"
-# echo Path to VMX is "$vmx_path"
+echo "+++ :packer: Building :buildkite: image from base"
+echo Path to VMX is "$vmx_path"
 
-# test -d output && rm -rf output/
-# packer build \
-# 	-var vmx_path="$vmx_path" \
-# 	-var packer_headless=true \
-# 	-var packer_output_dir=output/buildkite-macos \
-# 	macos-buildkite.json
-
-# if ! ls output/buildkite-macos/*.vmdk &> /dev/null ; then
-# 	echo "Failed to find any vmdk files in output/buildkite-macos"
-# 	exit 1
-# fi
-
-# upload_path="vmkite/buildkite-macOS-${osx_version}/build-$BUILDKITE_BUILD_NUMBER"
-
-# echo "+++ Uploading to $upload_path"
-# find output/buildkite-macos -name '*.vmdk' -print -execdir \
-# 	govc datastore.upload {} "${upload_path}/{}" \;
+test -d output && rm -rf output/
+packer build \
+	-var vmx_path="$vmx_path" \
+	-var packer_headless=true \
+	-var packer_output_dir=output/buildkite-macos \
+	-var osx_version="$osx_version" \
+	-var packer_headless=true \
+	-var vsphere_username="$GOVC_USERNAME" \
+	-var vsphere_password="$GOVC_PASSWORD" \
+	-var build_number="$BUILDKITE_BUILD_NUMBER" \
+	macos-buildkite.json
