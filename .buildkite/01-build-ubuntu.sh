ac42262c72b06714df8629d05286445a299dc539
Lachlan Donald <lachlan@strongwater.com.au>
2017-03-07 14:13:38 +1100

    Use more modern ubuntu packer config

--- .buildkite/01-build-ubuntu.sh
+++ .buildkite/01-build-ubuntu.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 echo "--- :packer: Building base :ubuntu: 16.04"
 packer build \
-	-var packer_headless=false \
-	-var vsphere_username="$GOVC_USERNAME" \
-	-var vsphere_password="$GOVC_PASSWORD" \
-	-var build_number="$BUILDKITE_BUILD_NUMBER" \
-	-var esxi_host="10.92.157.10" \
+	-var headless=false \
+	# -var vsphere_username="$GOVC_USERNAME" \
+	# -var vsphere_password="$GOVC_PASSWORD" \
+	# -var build_number="$BUILDKITE_BUILD_NUMBER" \
+	# -var esxi_host="10.92.157.10" \
 	ubuntu-1604-base.json
--- README.md
+++ README.md
@@ -9,3 +9,5 @@ References
 - https://github.com/nickcharlton/packer-esxi
 - https://github.com/timsutton/osx-vm-templates
 - https://github.com/boxcutter/macos
+- https://github.com/geerlingguy/packer-ubuntu-1604
+- https://github.com/kaoriatz/packer-templates
