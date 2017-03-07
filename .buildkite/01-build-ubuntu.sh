301327f4a243fc51ed9677fdcc83dc5955efe28d
Lachlan Donald <lachlan@strongwater.com.au>
2017-03-07 13:49:47 +1100

    headless=false

--- .buildkite/01-build-ubuntu.sh
+++ .buildkite/01-build-ubuntu.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 echo "--- :packer: Building base :ubuntu: 16.04"
 packer build \
-	-var packer_headless=true \
+	-var packer_headless=false \
 	-var vsphere_username="$GOVC_USERNAME" \
 	-var vsphere_password="$GOVC_PASSWORD" \
 	-var build_number="$BUILDKITE_BUILD_NUMBER" \
