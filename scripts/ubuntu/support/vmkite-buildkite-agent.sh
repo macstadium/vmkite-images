926099a24ed5650bf235623ddb1e86cda6dcd7ac
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-02 11:08:39 +1000

    Use --disconnect-after-job in ubuntu agent script

--- scripts/ubuntu/support/vmkite-buildkite-agent.sh
+++ scripts/ubuntu/support/vmkite-buildkite-agent.sh
@@ -8,8 +8,8 @@ VMWARE_TOOLS=/etc/vmware-tools
 
 guestinfo() {
   local key="guestinfo.$1"
-  local value=$(${VMWARE_TOOLS}/vmware-tools-daemon --cmd "info-get $key")
-  if [[ -n $value ]]; then
+  local value
+  if value=$(${VMWARE_TOOLS}/vmware-tools-daemon --cmd "info-get $key") ; then
     echo "$value"
   else
     echo >&2 "Missing $key"
@@ -27,8 +27,7 @@ echo "--- Starting buildkite-agent"
 export BUILDKITE_AGENT_TOKEN="$token"
 export BUILDKITE_AGENT_NAME="$name"
 export BUILDKITE_AGENT_META_DATA="vmkite-vmdk=$vmdk"
-export BUILDKITE_BUILD_PATH="/Users/vmkite/buildkite-builds"
 
-su vmkite -c "/usr/local/bin/buildkite-agent start"
+su vmkite -c "/usr/local/bin/buildkite-agent start --disconnect-after-job"
 echo "--- Buildkite exited with $?, shutting down machine"
 shutdown -h now
