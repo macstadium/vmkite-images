707d7d8aa5f77850d4019af730693d23f82e2f5c
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-02 11:04:49 +1000

    Make everything vmkite rather than vagrant

--- scripts/ubuntu/support/vmkite-buildkite-agent.sh
+++ scripts/ubuntu/support/vmkite-buildkite-agent.sh
@@ -4,10 +4,11 @@ set -e
 set -o pipefail
 set -u
 
+VMWARE_TOOLS=/etc/vmware-tools
+
 guestinfo() {
   local key="guestinfo.$1"
-  local tool="/Library/Application Support/VMware Tools/vmware-tools-daemon"
-  local value=$("$tool" --cmd "info-get $key")
+  local value=$(${VMWARE_TOOLS}/vmware-tools-daemon --cmd "info-get $key")
   if [[ -n $value ]]; then
     echo "$value"
   else
@@ -26,9 +27,8 @@ echo "--- Starting buildkite-agent"
 export BUILDKITE_AGENT_TOKEN="$token"
 export BUILDKITE_AGENT_NAME="$name"
 export BUILDKITE_AGENT_META_DATA="vmkite-vmdk=$vmdk"
-export BUILDKITE_BOOTSTRAP_SCRIPT_PATH="/Users/vmkite/buildkite-agent/bootstrap.sh"
 export BUILDKITE_BUILD_PATH="/Users/vmkite/buildkite-builds"
-su vmkite -c "/usr/local/bin/buildkite-agent start"
 
+su vmkite -c "/usr/local/bin/buildkite-agent start"
 echo "--- Buildkite exited with $?, shutting down machine"
 shutdown -h now
