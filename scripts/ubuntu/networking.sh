858a612f7dc27b8ce80b3fcbd3ef5046fab29db0
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-04 09:48:42 +1000

    Explicitly use ens160 as a network interface in ubuntu

--- scripts/ubuntu/networking.sh
+++ scripts/ubuntu/networking.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -eux
 
+# Not sure why, but when spawned by vmkite, the network interface is ens160
+# Suspect this is a variant of the ens33/ens32 issue elsewhere
+sed -i "s/ens32/ens160/g" /etc/network/interfaces
+
 # Add delay to prevent "vagrant reload" from failing
 echo "pre-up sleep 5" >> /etc/network/interfaces
