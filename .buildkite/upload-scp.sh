b2836c9f9ee3458aa1f5840c2bc6b8a031a1d96e
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-15 16:04:29 +1000

    Find output rather than doing uploading

--- .buildkite/upload-scp.sh
+++ .buildkite/upload-scp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-ls -al output/
+find output -name '*.vmdk'
 
-scp -v -r -P \
-  "${VMKITE_SCP_PORT}" \
-  "${BUILD_ARTIFACT}" \
-  "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}:${VMKITE_SCP_PATH}"
+# scp -v -r -P \
+#   "${VMKITE_SCP_PORT}" \
+#   "${BUILD_ARTIFACT}" \
+#   "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}:${VMKITE_SCP_PATH}"
