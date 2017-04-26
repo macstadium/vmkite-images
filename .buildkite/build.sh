102f9b672b32cd239056e430b654d9ec19d75b3f
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-26 18:25:39 +1000

    Add upload step

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -28,6 +28,8 @@ image="$1"
 filehash="$(get_hash_for_image "$image")"
 outputdir=$OUTPUT_DIR
 
+echo "+++ Building $image"
+
 if [[ -n $filehash ]] && [[ -e "$HASHES_DIR/${filehash}" ]] ; then
   outputdir=$(readlink "$HASHES_DIR/${filehash}")
   echo "Image is already built with hash of $filehash, points to $outputdir"
@@ -36,7 +38,7 @@ else
 fi
 
 if [[ ! -e "$outputdir" ]] ; then
-  echo "No build found at $HASHES_DIR/${filehash}, building"
+  echo "No build found at $HASHES_DIR/${filehash}"
   make "$@" "output_directory=$outputdir"
 
   if [[ -n "${filehash}" ]] ; then
@@ -46,10 +48,16 @@ if [[ ! -e "$outputdir" ]] ; then
   fi
 fi
 
-base_name=$(find "$outputdir" -iname '*.vmx' -exec basename {} \; | head -n1 | sed 's/\.vmx//')
-upload_path="${VMKITE_SCP_PATH}/${base_name}-r${BUILDKITE_BUILD_NUMBER:-0}"
+vm_name=$(find "$outputdir" -iname '*.vmx' -exec basename {} \; | head -n1 | sed 's/\.vmx//')
+upload_path="${VMKITE_SCP_PATH}/${vm_name}"
+sftp_script=$(cat <<KITTENS
+mkdir $upload_path
+cd $upload_path
+put -ra .
+KITTENS
+)
 
 cd "$outputdir"
 echo "+++ Uploading $outputdir to $upload_path"
-# sftp -b <(printf "put -r "${disk}/" "${remote_path}") \
-#   -P"${VMKITE_SCP_PORT}" "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}"
+sftp -b <(echo "$sftp_script") \
+  -P"${VMKITE_SCP_PORT}" "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}"
