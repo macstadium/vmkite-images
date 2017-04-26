e0ed90bec58c43bdd6f3830d054820e71cc8fc31
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-26 18:33:40 +1000

    Do the upload

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -24,6 +24,18 @@ get_hash_for_image() {
   esac
 }
 
+find_vm_name() {
+  find "$1" -iname '*.vmx' -exec basename {} \; | head -n1 | sed 's/\.vmx//'
+}
+
+upload_to_sftp() {
+  local sourcedir="$1"
+  upload_path="${VMKITE_SCP_PATH}/$(find_vm_name "$sourcedir")"
+
+  sftp -b <(echo "lcd $outputdir; mkdir $upload_path; cd $upload_path; put -r .") \
+    -P"${VMKITE_SCP_PORT}" "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}"
+}
+
 image="$1"
 filehash="$(get_hash_for_image "$image")"
 outputdir=$OUTPUT_DIR
@@ -40,24 +52,13 @@ fi
 if [[ ! -e "$outputdir" ]] ; then
   echo "No build found at $HASHES_DIR/${filehash}"
   make "$@" "output_directory=$outputdir"
-
-  if [[ -n "${filehash}" ]] ; then
-    echo "Linking hash of ${filehash} to ${outputdir}"
-    mkdir -p "$HASHES_DIR"
-    ln -sf "${HASHES_DIR}/${filehash}" "${outputdir}"
-  fi
 fi
 
-vm_name=$(find "$outputdir" -iname '*.vmx' -exec basename {} \; | head -n1 | sed 's/\.vmx//')
-upload_path="${VMKITE_SCP_PATH}/${vm_name}"
-sftp_script=$(cat <<KITTENS
-mkdir $upload_path
-cd $upload_path
-put -r .
-KITTENS
-)
+echo "+++ Uploading $outputdir to sftp"
+upload_to_sftp "$outputdir"
 
-cd "$outputdir"
-echo "+++ Uploading $outputdir to $upload_path"
-sftp -b <(echo "$sftp_script") \
-  -P"${VMKITE_SCP_PORT}" "${VMKITE_SCP_USER}@${VMKITE_SCP_HOST}"
+if [[ -n "${filehash}" ]] ; then
+  echo "--- Linking hash ${filehash} to ${outputdir}"
+  mkdir -p "$HASHES_DIR"
+  ln -sf "${HASHES_DIR}/${filehash}" "${outputdir}"
+fi
