1f94ee30fb998827338be706a0bc03fd98f0a4eb
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-11 17:45:38 +1000

    Enable auto-login and system updates

--- scripts/macos/autologin.sh
+++ scripts/macos/autologin.sh
@@ -1,9 +1,14 @@
-if [ "$AUTOLOGIN" != "true" ] && [ "$AUTOLOGIN" != "1" ]; then
-  exit
-fi
 
 echo "Enabling automatic GUI login for the '$USERNAME' user.."
 
 python /private/tmp/set_kcpassword.py "$PASSWORD"
 
 /usr/bin/defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser "$USERNAME"
+
+echo "Disabling screensaver.."
+
+/usr/bin/defaults -currentHost write com.apple.screensaver idleTime 0
+
+echo "Disabling sleep.."
+
+pmset -a sleep 0
--- scripts/macos/system-update.sh
+++ scripts/macos/system-update.sh
@@ -1,6 +1,3 @@
-if [ "$UPDATE_SYSTEM" != "true" ] && [ "$UPDATE_SYSTEM" != "1" ]; then
-  exit
-fi
 
 echo "Downloading and installing system updates..."
 softwareupdate -i -a
