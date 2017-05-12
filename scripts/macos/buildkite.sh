42caf82fad05a1b4bbd6ba667f3d38c5b0df7a74
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-12 19:24:23 +1000

    Add debugging to buildkite-agent download

--- scripts/macos/buildkite.sh
+++ scripts/macos/buildkite.sh
@@ -2,17 +2,13 @@
 set -eux
 
 PROVISION_DIR="$HOME"
-BUILDKITE_VERSION=3.0-beta.19
-
-download() {
-  local file="$1"
-  local url="$2"
-  curl -Lfs --retry 10 -o "${file}" "${url}"
-}
+BUILDKITE_VERSION=3.0-beta.23
 
 install_buildkite() {
   echo "Installing buildkite-agent"
-  download buildkite.tar.gz "https://github.com/buildkite/agent/releases/download/v${BUILDKITE_VERSION}/buildkite-agent-darwin-amd64-${BUILDKITE_VERSION}.tar.gz"
+  curl -Lf \
+    -o buildkite.tar.gz \
+    "https://github.com/buildkite/agent/releases/download/v${BUILDKITE_VERSION}/buildkite-agent-darwin-amd64-${BUILDKITE_VERSION}.tar.gz"
   mkdir -p "$HOME/buildkite-agent"
   tar -xzf buildkite.tar.gz -C "$HOME/buildkite-agent"
   sudo mkdir -p /usr/local/bin
