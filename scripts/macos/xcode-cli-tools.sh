f8e0443790c4e5d3cc22718e6fcb76e9e67a4c91
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-14 20:45:36 +1000

    Add back xcode cli tools install

---
+++ scripts/macos/xcode-cli-tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+
+# create the placeholder file that's checked by CLI updates' .dist code
+# in Apple's SUS catalog
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
+# find the CLI Tools update
+PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+
+# install it
+softwareupdate -i "$PROD" --verbose
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
