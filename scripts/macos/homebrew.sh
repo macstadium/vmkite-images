9ac02d375f14b3020d2112676278a2fa1d589815
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-14 19:26:34 +1000

    Try hardcoding the user for su / homebrew install

--- scripts/macos/homebrew.sh
+++ scripts/macos/homebrew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 
-su - "$USERNAME" -c \
+su - vmkite -c \
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
--- scripts/macos/shrink.sh
+++ scripts/macos/shrink.sh
@@ -3,15 +3,13 @@ set -eux
 
 du -d 2 -h /Applications
 
-OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
-
 # Turn off hibernation and get rid of the sleepimage
 pmset hibernatemode 0
 rm -f /var/vm/sleepimage
 
 # Stop the pager process and drop swap files. These will be re-created on boot.
 # Starting with El Cap we can only stop the dynamic pager if SIP is disabled.
-if [ "$OSX_VERS" -lt 11 ] || $(csrutil status | grep -q disabled); then
+if csrutil status | grep -q disabled; then
     launchctl unload /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist
     sleep 5
 fi
