27c05af50605aa0e8ee7d4fe1fd40ebb548d92b7
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-15 20:22:05 +1000

    Install xcversion in the macOS base image

--- scripts/macos/xcode.sh
+++ scripts/macos/xcode.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-gem env
 gem install xcode-install
+
+export XCODE_INSTALL_USER=$FASTLANE_USER
+export XCODE_INSTALL_PASSWORD=$FASTLANE_PASSWORD
+
+/usr/local/bin/xcversion list
 /usr/local/bin/xcversion install "$XCODE_VERSION"
