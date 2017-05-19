97a3a00af78717aad7800fbb0d1ad95919cfbd02
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-19 15:22:40 +1000

    Remove xcode install cache after it's done

--- scripts/macos/xcode.sh
+++ scripts/macos/xcode.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 export XCODE_INSTALL_USER=$FASTLANE_USER
 export XCODE_INSTALL_PASSWORD=$FASTLANE_PASSWORD
+export XCODE_INSTALL_CACHE=$HOME/Library/Caches/XcodeInstall
 
 /usr/local/bin/xcversion list
 /usr/local/bin/xcversion install "$XCODE_VERSION"
+
+rm -rf "$XCODE_INSTALL_CACHE"
