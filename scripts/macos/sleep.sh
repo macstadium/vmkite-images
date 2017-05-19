818a5a1a0f0ea122bfd6de6984680c69cd4d5ce9
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-20 08:11:26 +1000

    Copy Xcode from the host

--- scripts/macos/sleep.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# See https://github.com/timsutton/osx-vm-templates/issues/35
-sleep 5
--- scripts/macos/xcode.sh
+++ scripts/macos/xcode.sh
@@ -5,7 +5,9 @@ export XCODE_INSTALL_USER=$FASTLANE_USER
 export XCODE_INSTALL_PASSWORD=$FASTLANE_PASSWORD
 export XCODE_INSTALL_CACHE=$HOME/Library/Caches/XcodeInstall
 
-/usr/local/bin/xcversion list
-/usr/local/bin/xcversion install "$XCODE_VERSION"
+# /usr/local/bin/xcversion list
+# /usr/local/bin/xcversion install "$XCODE_VERSION"
 
-rm -rf "$XCODE_INSTALL_CACHE"
+# rm -rf "$XCODE_INSTALL_CACHE"
+
+ls -al /Applications
