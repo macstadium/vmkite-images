254619048e534557b60c756e44f4754a4beecfae
Lachlan Donald <lachlan@strongwater.com.au>
2017-05-21 13:00:13 +1000

    Iterate on uploading xcode tar file

--- scripts/macos/xcode.sh
+++ scripts/macos/xcode.sh
@@ -5,6 +5,8 @@ export XCODE_INSTALL_USER=$FASTLANE_USER
 export XCODE_INSTALL_PASSWORD=$FASTLANE_PASSWORD
 export XCODE_INSTALL_CACHE=$HOME/Library/Caches/XcodeInstall
 
+ls -alR /tmp
+
 # /usr/local/bin/xcversion list
 # /usr/local/bin/xcversion install "$XCODE_VERSION"
 
