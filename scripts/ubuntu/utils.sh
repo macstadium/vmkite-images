b8305437ee58885f32b68a9bcbe2c8830d364db4
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-01 10:52:16 +1000

    Install awscli via apt 🎉

--- scripts/ubuntu/utils.sh
+++ scripts/ubuntu/utils.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -eux
 
-# aws-cli
-apt-get -y install python-pip
-pip install --upgrade --user awscli
-ln -s /usr/local/bin/aws ~/.local/bin/aws
-
-# jq
-apt-get install jq
+apt-get install awscli jq
