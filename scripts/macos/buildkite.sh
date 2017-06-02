8bd6364786fc318c3c3d7ee37e59a6ed6b8f00d1
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-02 10:31:28 +1000

    Homebrew needs to run as vmkite user

--- scripts/macos/buildkite.sh
+++ scripts/macos/buildkite.sh
@@ -3,10 +3,6 @@ set -eux
 
 PROVISION_DIR="$HOME"
 
-echo $PATH
-
-# echo export PATH="/usr/local/bin:$PATH" >> ~/.bash_profile
-
 install_buildkite() {
   echo "Installing buildkite-agent"
   /usr/local/bin/brew tap buildkite/buildkite
@@ -18,11 +14,16 @@ install_launchd_daemon() {
   local script="vmkite-buildkite-agent.sh"
   local plist="com.macstadium.vmkite-buildkite-agent.plist"
   echo "Installing launchd service"
-  cp "${PROVISION_DIR}/$script" "/usr/local/bin/$script"
-  cp "${PROVISION_DIR}/$plist" "/Library/LaunchDaemons/$plist"
+  sudo cp "${PROVISION_DIR}/$script" "/usr/local/bin/$script"
+  sudo cp "${PROVISION_DIR}/$plist" "/Library/LaunchDaemons/$plist"
   sudo chmod 0755 "/usr/local/bin/$script"
   sudo launchctl load "/Library/LaunchDaemons/$plist"
 }
 
+install_utils() {
+  /usr/local/bin/brew install awscli jq
+}
+
+install_utils
 install_buildkite
 install_launchd_daemon
