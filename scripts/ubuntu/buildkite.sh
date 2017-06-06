fc0d8aae1be9d20dffa8dfcc861aa5b05a001eea
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-06 16:22:48 +1000

    Need an install for git-lfs on ubuntu

--- scripts/ubuntu/buildkite.sh
+++ scripts/ubuntu/buildkite.sh
@@ -20,6 +20,7 @@ install_service() {
 
 install_utils() {
   curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+  sudo apt-get install git-lfs
   git lfs install
 }
 
