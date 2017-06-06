7075fd514c0a61d137ff1ec6e19925b00671c232
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-06 20:55:59 +1000

    Go back to non-compiled xcversion install

--- scripts/macos/xcode-cli-tools.sh
+++ scripts/macos/xcode-cli-tools.sh
@@ -2,7 +2,10 @@
 set -eux
 
 # https://github.com/KrauseFx/xcode-install#installation
-sudo gem install xcode-install
+curl -sL -O https://github.com/neonichu/ruby-domain_name/releases/download/v0.5.99999999/domain_name-0.5.99999999.gem
+sudo gem install domain_name-0.5.99999999.gem
+sudo gem install --conservative xcode-install
+rm -f domain_name-0.5.99999999.gem
 
 # Install cli tools
 /usr/local/bin/xcversion install-cli-tools
