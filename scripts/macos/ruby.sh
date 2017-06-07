c59ec84b4db7e93d6fc30cda2aac2ed8c77bb2e1
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-07 16:01:32 +1000

    Clean up ruby install, test locallyt

--- scripts/macos/ruby.sh
+++ scripts/macos/ruby.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
+set -euo pipefail
 
 export RBENV_ROOT="$HOME/.rbenv"
 export RBENV_PLUGINS="$RBENV_ROOT/plugins"
+export PATH="$RBENV_ROOT/bin:/usr/local/bin/$PATH"
 
-git clone https://github.com/sstephenson/rbenv.git "$RBENV_ROOT"
-
-export PATH="$RBENV_ROOT/bin:$PATH"
-echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-
-eval "$(rbenv init -)"
+echo 'export PATH="~/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
-cat << EOF > $RBENV_ROOT/default-gems
+git clone https://github.com/sstephenson/rbenv.git "$RBENV_ROOT"
+eval "$(rbenv init -)"
+
+cat << EOF > "$RBENV_ROOT/default-gems"
 bundler
-rubygems-bundler
 EOF
 
-mkdir -p $RBENV_PLUGINS
+mkdir -p "$RBENV_PLUGINS"
+git clone https://github.com/garnieretienne/rvm-download.git    "$RBENV_PLUGINS/rvm-download"
+git clone https://github.com/sstephenson/ruby-build.git         "$RBENV_PLUGINS/ruby-build"
+git clone https://github.com/sstephenson/rbenv-default-gems.git "$RBENV_PLUGINS/rbenv-default-gems"
+git clone https://github.com/rkh/rbenv-whatis.git               "$RBENV_PLUGINS/rbenv-whatis"
+git clone https://github.com/rkh/rbenv-use.git                  "$RBENV_PLUGINS/rbenv-use"
+git clone https://github.com/tpope/rbenv-communal-gems.git      "$RBENV_PLUGINS/rbenv-communal-gems"
+git clone https://github.com/nicknovitski/rbenv-gem-update      "$RBENV_PLUGINS/rbenv-gem-update"
 
-git clone https://github.com/sstephenson/ruby-build.git         $RBENV_PLUGINS/ruby-build
-git clone https://github.com/sstephenson/rbenv-default-gems.git $RBENV_PLUGINS/rbenv-default-gems
-git clone https://github.com/rkh/rbenv-whatis.git               $RBENV_PLUGINS/rbenv-whatis
-git clone https://github.com/rkh/rbenv-use.git                  $RBENV_PLUGINS/rbenv-use
-git clone https://github.com/tpope/rbenv-communal-gems.git      $RBENV_PLUGINS/rbenv-communal-gems
-git clone https://github.com/nicknovitski/rbenv-gem-update      $RBENV_PLUGINS/rbenv-gem-update
-
-/usr/local/bin/brew install openssl libyaml libffi
+export HOMEBREW_NO_AUTO_UPDATE=1
+brew install openssl libyaml libffi
 
 rbenv install 2.3.1
-rbenv alias --auto
 rbenv use 2.3.1 --global
 rbenv communize --all
--- scripts/macos/xcode.sh
+++ scripts/macos/xcode.sh
@@ -2,12 +2,11 @@
 set -euo pipefail
 source ~/.bash_profile
 
-gem install xcode-install
-
 if [ -z "${XCODE_VERSION:-}" ] ; then
   echo "Must set \$XCODE_VERSION"
   exit 1
 fi
 
+gem install xcode-install
 xcversion install "$XCODE_VERSION"
 xcversion cleanup
