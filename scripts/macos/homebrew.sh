d17d32d5c7ac126ada6d9b21d904b21913e70106
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-07 20:08:29 +1000

    Iterate on homebrew and ruby install

--- scripts/macos/homebrew.sh
+++ scripts/macos/homebrew.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -eux
 
-# Install xcode tools
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" -v;
-
 curl -fsSL -o install-homebrew \
   https://raw.githubusercontent.com/Homebrew/install/master/install
 
--- scripts/macos/ruby.sh
+++ scripts/macos/ruby.sh
@@ -3,30 +3,26 @@ set -euo pipefail
 
 export RBENV_ROOT="$HOME/.rbenv"
 export RBENV_PLUGINS="$RBENV_ROOT/plugins"
-export PATH="$RBENV_ROOT/bin:/usr/local/bin:$PATH"
-
-echo 'export PATH="~/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone https://github.com/sstephenson/rbenv.git "$RBENV_ROOT"
-eval "$(rbenv init -)"
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
 
 cat << EOF > "$RBENV_ROOT/default-gems"
 bundler
 EOF
 
 mkdir -p "$RBENV_PLUGINS"
-git clone https://github.com/garnieretienne/rvm-download.git    "$RBENV_PLUGINS/rvm-download"
 git clone https://github.com/sstephenson/ruby-build.git         "$RBENV_PLUGINS/ruby-build"
 git clone https://github.com/sstephenson/rbenv-default-gems.git "$RBENV_PLUGINS/rbenv-default-gems"
-git clone https://github.com/rkh/rbenv-whatis.git               "$RBENV_PLUGINS/rbenv-whatis"
-git clone https://github.com/rkh/rbenv-use.git                  "$RBENV_PLUGINS/rbenv-use"
 git clone https://github.com/tpope/rbenv-communal-gems.git      "$RBENV_PLUGINS/rbenv-communal-gems"
-git clone https://github.com/nicknovitski/rbenv-gem-update      "$RBENV_PLUGINS/rbenv-gem-update"
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew install openssl libyaml libffi
 
 rbenv install 2.3.1
-rbenv use 2.3.1 --global
+rbenv global 2.3.1
 rbenv communize --all
+rbenv versions
