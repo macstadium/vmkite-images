caeee328b4c7927fd0992817b241cb7461b04daa
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-08 10:18:57 +1000

    Use dash headers rather than plus headers

--- .buildkite/format-packer.sh
+++ .buildkite/format-packer.sh
@@ -2,9 +2,9 @@
 
 sed -E '
   s/^(==>|   ) vmware-(iso|vmx): ?//g;
-  s/^(Downloading or copying ISO)/+++ :packer: \1/g;
-  s/^(Cloning source VM)/+++ :packer: \1/g;
-  s/^(Provisioning with shell script)/+++ :packer: \1/g;
-  s/^(Starting virtual machine)/+++ :packer: \1/g;
-  s/^(Waiting for SSH to become available)/+++ :packer: \1/g;
+  s/^(Downloading or copying ISO)/--- :packer: \1/g;
+  s/^(Cloning source VM)/--- :packer: \1/g;
+  s/^(Provisioning with shell script)/--- :packer: \1/g;
+  s/^(Starting virtual machine)/--- :packer: \1/g;
+  s/^(Waiting for SSH to become available)/--- :packer: \1/g;
 '
