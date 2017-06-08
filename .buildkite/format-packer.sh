f7d7ba242bc7fde6b37c538340b73bf09d6555e0
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-08 10:18:27 +1000

    Add more formatting headers in build

--- .buildkite/format-packer.sh
+++ .buildkite/format-packer.sh
@@ -5,4 +5,6 @@ sed -E '
   s/^(Downloading or copying ISO)/+++ :packer: \1/g;
   s/^(Cloning source VM)/+++ :packer: \1/g;
   s/^(Provisioning with shell script)/+++ :packer: \1/g;
+  s/^(Starting virtual machine)/+++ :packer: \1/g;
+  s/^(Waiting for SSH to become available)/+++ :packer: \1/g;
 '
