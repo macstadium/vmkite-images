8729a7d68741e09662c7043674a55473643c98bd
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-10 15:35:26 +1000

    Try ahci disks on mac

--- scripts/macos/homebrew.sh
+++ scripts/macos/homebrew.sh
@@ -8,8 +8,7 @@ source ~/.bash_profile
 curl -fsSL -o install-homebrew \
   https://raw.githubusercontent.com/Homebrew/install/master/install
 
-chmod +x install-homebrew
-./install-homebrew
-rm install-homebrew
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew tap Homebrew/bundle
