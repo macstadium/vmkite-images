9e0b80ebb09c1822d10201aec34170f2c08d9abb
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-15 20:48:52 +1000

    Push vmkite-version into vm's

--- scripts/ubuntu/support/vmkite-wrapper.sh
+++ scripts/ubuntu/support/vmkite-wrapper.sh
@@ -37,6 +37,7 @@ VMKITE_VSPHERE_HOST="$(guestinfo vmkite-vsphere-host)"
 VMKITE_VSPHERE_INSECURE="$(guestinfo vmkite-vsphere-insecure 'true')"
 VMKITE_VSPHERE_PASS="$(guestinfo vmkite-vsphere-pass)"
 VMKITE_VSPHERE_USER="$(guestinfo vmkite-vsphere-user)"
+VMKITE_VERSION="$(/usr/local/bin/vmkite --version)"
 
 export VMKITE_VSPHERE_HOST
 export VMKITE_VSPHERE_INSECURE
@@ -58,6 +59,7 @@ export VMKITE_VM_AWS_ACCESS_KEY_ID
 export VMKITE_VM_AWS_SECRET_ACCESS_KEY
 
 exec /usr/local/bin/vmkite run \
+  --vm-guest-info "vmkite-version=${VMKITE_VERSION}" \
   --vm-guest-info "vmkite-buildkite-debug=${VMKITE_BUILDKITE_DEBUG}" \
   --vm-guest-info "aws-access-key-id=${VMKITE_VM_AWS_ACCESS_KEY_ID}" \
   --vm-guest-info "aws-secret-access-key=${VMKITE_VM_AWS_SECRET_ACCESS_KEY}" \
