266e5717576e97a94e59293ee0e7c1d15acebd82
Lachlan Donald <lachlan@strongwater.com.au>
2017-06-23 11:11:47 +1000

    Manually install xcode dev tools before homebrew

--- scripts/macos/homebrew.sh
+++ scripts/macos/homebrew.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-set -eux
+set -eu
 
 # Add homebrew to path
 echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile
 source ~/.bash_profile
 
-curl -fsSL -o install-homebrew \
-  https://raw.githubusercontent.com/Homebrew/install/master/install
+# Manually install xcode dev tools, it fails sometimes in the homebrew installer
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
 
-chmod +x install-homebrew
-./install-homebrew
-rm install-homebrew
-
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Homebrew/bundle
