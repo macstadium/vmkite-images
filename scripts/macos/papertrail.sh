58727c77132fb716c0661eb79087c774df3ed3b2
Lachlan Donald <lachlan@strongwater.com.au>
2017-07-02 13:41:10 +1000

    Use correct extracted folder for remote_syslog2

--- scripts/macos/papertrail.sh
+++ scripts/macos/papertrail.sh
@@ -7,9 +7,10 @@ REMOTE_SYSLOG_VERSION="v0.20-beta1"
 install_remote_syslog2() {
   echo "Installing remote_syslog2"
   curl -Lfs -o remote_syslog2.tar.gz https://github.com/papertrail/remote_syslog2/releases/download/${REMOTE_SYSLOG_VERSION}/remote_syslog_darwin_amd64.tar.gz
-  tar xzvf  --strip-components 1 remote_syslog2.tar.gz
-  chmod +x remote_syslog
+  tar xzvf remote_syslog2.tar.gz
+  chmod +x remote_syslog/remote_syslog
   sudo mv remote_syslog /usr/local/bin/remote_syslog
+  rm -rf remote_syslog
 }
 
 install_launchd_daemon() {
