076695e8347ddd7727034b9bf4cba2e6655a477d
Lachlan Donald <lachlan@strongwater.com.au>
2017-07-02 13:37:39 +1000

    Strip components when untarring

--- scripts/macos/papertrail.sh
+++ scripts/macos/papertrail.sh
@@ -7,7 +7,7 @@ REMOTE_SYSLOG_VERSION="v0.20-beta1"
 install_remote_syslog2() {
   echo "Installing remote_syslog2"
   curl -Lfs -o remote_syslog2.tar.gz https://github.com/papertrail/remote_syslog2/releases/download/${REMOTE_SYSLOG_VERSION}/remote_syslog_darwin_amd64.tar.gz
-  tar xzvf remote_syslog2.tar.gz
+  tar xzvf  --strip-components 1 remote_syslog2.tar.gz
   chmod +x remote_syslog
   sudo mv remote_syslog /usr/local/bin/remote_syslog
 }
