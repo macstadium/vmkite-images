cc8a7b0ea38ad8abc5e5d82ca381e6acebb2da2f
Lachlan Donald <lachlan@strongwater.com.au>
2017-07-14 14:37:27 +1000

    Turns out you need to quote 'EOF'

--- scripts/macos/hostname.sh
+++ scripts/macos/hostname.sh
@@ -2,18 +2,17 @@
 set -eux
 
 # This script adds a Mac OS Launch Daemon, which runs on first boot
-PLIST=/Library/LaunchDaemons/com.macstadium.set-hostname.plist
+PLIST=/Library/LaunchDaemons/com.macstadium.vmkite.set-random-hostname.plist
 cat <<EOF > "${PLIST}"
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
     <key>Label</key>
-    <string>com.github.timsutton.osx-vm-templates.detectnewhardware</string>
+    <string>com.macstadium.vmkite.set-random-hostname</string>
     <key>ProgramArguments</key>
     <array>
-        <string>/usr/sbin/networksetup</string>
-        <string>-detectnewhardware</string>
+        <string>/usr/local/bin/set-random-hostname</string>
     </array>
     <key>RunAtLoad</key>
     <true/>
@@ -22,10 +21,11 @@ cat <<EOF > "${PLIST}"
 EOF
 
 # This script sets a random hostname
-cat <<-EOF > /usr/local/bin/set-random-hostname
+cat << 'EOF' > /usr/local/bin/set-random-hostname
 #!/bin/bash
 set -eu
-scutil –-set HostName "$(sed "$(perl -e "print int rand(99999)")q;d" /usr/share/dict/words | awk '{print tolower($0)}')"
+RANDOM_WORDS="$(sed "$(perl -e "print int rand(99999)")q;d" /usr/share/dict/words | awk '{print tolower($0)}')"
+scutil –-set HostName "vmkite-${RANDOM_WORDS}"
 EOF
 
 /bin/chmod 644 "${PLIST}" "/usr/local/bin/set-random-hostname"
