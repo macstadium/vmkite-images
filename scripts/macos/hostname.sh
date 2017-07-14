9966750f823ca84321a4b81d0bdecd5ba62eac88
Lachlan Donald <lachlan@strongwater.com.au>
2017-07-14 15:00:28 +1000

    Perhaps multi EOF's are a problem?

--- scripts/macos/hostname.sh
+++ scripts/macos/hostname.sh
@@ -3,7 +3,7 @@ set -eux
 
 # This script adds a Mac OS Launch Daemon, which runs on first boot
 PLIST=/Library/LaunchDaemons/com.macstadium.vmkite.set-random-hostname.plist
-cat <<EOF > "${PLIST}"
+cat << EOF > "${PLIST}"
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -21,12 +21,13 @@ cat <<EOF > "${PLIST}"
 EOF
 
 # This script sets a random hostname
-cat << 'EOF' > /usr/local/bin/set-random-hostname
+SCRIPT=/usr/local/bin/set-random-hostname
+cat << 'SCRIPT_EOF' > "${SCRIPT}"
 #!/bin/bash
 set -eu
 RANDOM_WORDS="$(sed "$(perl -e "print int rand(99999)")q;d" /usr/share/dict/words | awk '{print tolower($0)}')"
 scutil –-set HostName "vmkite-${RANDOM_WORDS}"
-EOF
+SCRIPT_EOF
 
-/bin/chmod 644 "${PLIST}" "/usr/local/bin/set-random-hostname"
-/usr/sbin/chown root:wheel "${PLIST}" "/usr/local/bin/set-random-hostname"
+/bin/chmod 644 "${PLIST}" "${SCRIPT}"
+/usr/sbin/chown root:wheel "${PLIST}" "${SCRIPT}"
